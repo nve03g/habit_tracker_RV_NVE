07b35927315f667b4e28c1429fd0d07e486d44b1
Nellie Van Eeckhaute <nve03g@gmail.com>
2024-11-25 18:11:10 +0100

    hardcoded list data, button not yet functional

--- app/src/main/java/com/example/habit_tracker/MainActivity.kt
+++ app/src/main/java/com/example/habit_tracker/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.habit_tracker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+
+/*
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+*/
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        */
+
+        val habitListView: ListView = findViewById(R.id.habitListView)
+        val addHabitButton: Button = findViewById(R.id.addHabitButton)
+
+        // dummy list data
+        val habits = listOf("drink water", "exercise", "read a book")
+        val adapter = android.widget.ArrayAdapter(
+            this,
+            android.R.layout.simple_list_item_1,
+            habits
+        )
+        habitListView.adapter = adapter
+
+        // click button to add new habit
+        addHabitButton.setOnClickListener{
+            // here comes button functionality
         }
     }
 }
